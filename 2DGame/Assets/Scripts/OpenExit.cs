3b27e106ac6022700309adec75d3692e2445f661
WBorowski <wborowski@gmail.com>
2020-05-30 23:35:52 +0200

    Complete level

---
+++ 2DGame/Assets/Scripts/OpenExit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenExit : MonoBehaviour
+{
+    public SpriteRenderer exit;
+    public Sprite exitOpen;
+    public Sprite mySprite;
+    private SpriteRenderer toggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggle = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            toggle.sprite = mySprite;
+            exit.sprite = exitOpen;
+        }
+    }
+}
